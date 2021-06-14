68f27c1f168b72d7d9f6ebeb079c88d0c72beec9
Kwynn <buessresume@gmail.com>
2021-06-14 02:07:13 -0400

    measuring freq change

--- logs.php
+++ logs.php
@@ -9,11 +9,36 @@ class chrony_log_parse {
     public function __construct($file) {
 	$this->load10($file);
 	$this->do10();
-	// $this->do20();
+	$this->do20();
+    }
+    
+    private function do20() {
+	$a = $this->lpa10;
+	$fi = 0;
+	$n = count($a);
+	if ($n < 2) return;
+	$li = $n - 1;
+	$spans = $a[$fi]['ts'] - $a[$li]['ts']; 
+	
+	$change = 0;
+	for($i=0; $i < $li; $i++) $change += abs($a[$i  ]['freq_corr'] - 
+					         $a[$i+1]['freq_corr']);
+	
+
+	unset($fi, $li, $i, $a);
+	
+	$change_freq_corr = $change; unset($change);
+	
+	$this->an10 = get_defined_vars();
+	
+	return;
+	
+	
     }
     
     private function do10() {
 	$a = $this->linea;
+	$ret = [];
 	foreach($a as $l) {
 	    $t = [];
 	    $ds = $t['ds'] = $l[0] . ' ' . $l[1] . ' UTC';
@@ -21,8 +46,11 @@ class chrony_log_parse {
 	    $t['dss'] = date('h:i:s A', $ts);
 	    if (!preg_match('/^-?\d+\.\d+$/',$l[4], $ms)) continue;
 	    $t['freq_corr'] = floatval($ms[0]);
+	    $ret[] = $t;
 	    continue;
 	}
+	
+	$this->lpa10 = $ret;
     }
     
     private function load10($fpath) {
