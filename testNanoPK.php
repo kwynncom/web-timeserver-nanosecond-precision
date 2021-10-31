2217a57fcde568a7706f276f9d10b423f08537a3
kwynncom <buessresume@gmail.com>
2021-10-31 19:28:51 -0400

    modified nanopk test

--- testNanoPK.php
+++ testNanoPK.php
@@ -7,23 +7,10 @@ echo($name . ' version: ' . phpversion($name) . "\n");
 echo('PHP version: ' . PHP_VERSION . "\n");
 
 
-$fs = ['time', 'nanotime', 'nanopk', 'uptime', 'rdtscp', 'nanopkavg', 'nanotime_array'];
+$fs = ['time', 'nanotime'];
 foreach($fs as $f) var_dump($f());
 var_dump(nanopk(NANOPK_VERSION));
 
-if (1) {
-	$args = [
-		0,
-		NANOPK_U,
-		NANOPK_TSC,
-		NANOPK_PID | NANOPK_UNS,
-		NANOPK_ALL
-		];
+echo('this file version 2021/10/31 7:28pm' . "\n");
 
-	foreach($args as $a) var_dump(nanopk($a));
-}
-
-echo('PHP version: ' . PHP_VERSION . "\n");
-echo('test version: 0.3.2, this file version 2021/10/31 5:57pm' . "\n");
-
-// from https://github.com/kwynncom/nano-php-extension/blob/main/test.php
+// originally from https://github.com/kwynncom/nano-php-extension/blob/main/test.php
